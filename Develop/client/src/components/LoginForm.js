76b8686916601ea5aa1417d80ed328aa3578fef9
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-08-28 22:57:53 -0700

    deploying

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { LOGIN_USER } from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/react-hooks'
 
